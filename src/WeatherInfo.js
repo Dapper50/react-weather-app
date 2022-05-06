819477d05e9acdd192370cbfd42010c2db97fee9
Boma Dapper <94048274+Dapper50@users.noreply.github.com>
2022-05-06 14:01:00 +0100

    Updated styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,28 +6,36 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <h1>{props.data.city}</h1>
+          </div>
+          <div className="col-3">
+            <div className="damn-container d-flex justify-content-end">
+              <WeatherIcon code={props.data.icon} size={52} />
+
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+      </div>
       <ul>
         <li>
           <FormattedDate date={props.data.date} />
         </li>
+
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <div className="damn" mb-5>
-              <WeatherIcon code={props.data.icon} />
-            </div>
-            <WeatherTemperature celsius={props.data.temperature} />
+            <ul>
+              <li>
+                Humidity: {props.data.humidity}%, Wind: {props.data.wind} km/h
+              </li>
+            </ul>
           </div>
-        </div>
-
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
-          </ul>
         </div>
       </div>
     </div>
